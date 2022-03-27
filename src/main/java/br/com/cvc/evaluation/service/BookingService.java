00a5addad8595c2d6a5cf43e90f4e29586b4ae65
JOAO PAULO BRANDAO DA SILVA <joao.silva-ext@viavarejo.com.br>
2022-03-27 09:54:35 -0300

    Adding logs

--- src/main/java/br/com/cvc/evaluation/service/BookingService.java
+++ src/main/java/br/com/cvc/evaluation/service/BookingService.java
@@ -16,8 +16,10 @@ import br.com.cvc.evaluation.domain.Hotel;
 import br.com.cvc.evaluation.domain.Room;
 import br.com.cvc.evaluation.service.mapper.HotelMapper;
 import br.com.cvc.evaluation.service.mapper.RoomMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
+@Slf4j
 @ApplicationScoped
 public class BookingService {
     private static final Long ONE_DAY = Long.valueOf("1");
@@ -37,17 +39,21 @@ public class BookingService {
     RoomMapper roomMapper;
 
     private BigDecimal calculateTotalPrice(final BigDecimal paxPrice, final Long days) {
+        log.info("Calculating total price: pax price {} for {} days", paxPrice, days);
         final var fee = this.feeService.calculateFee(paxPrice, days);
 
         return paxPrice.add(fee).multiply(BigDecimal.valueOf(days));
     }
 
     private Long calculatePeriod(final LocalDate checkin, final LocalDate checkout) {
+        log.info("Calculating period: checking {}, checkout {}", checkin, checkout);
         return checkin.until(checkout, ChronoUnit.DAYS);
     }
 
     private Room calculateTotalPrice(final BrokerHotelRoom brokerHotelRoom, final Long days, final Integer adults,
                     final Integer child) {
+        log.info("Calculating total price: room {}, {} days, {} adults, {} child",
+                        brokerHotelRoom.getCategoryName(), days, adults, child);
         final var room = this.roomMapper.toDomain(brokerHotelRoom);
         var totalPrice = BigDecimal.ZERO;
 
@@ -63,6 +69,7 @@ public class BookingService {
             totalPrice = totalPrice.add(room.getPriceDetail().getPricePerDayChild().multiply(BigDecimal.valueOf(days)));
         }
 
+        log.info("Total price: {}", totalPrice);
         room.setTotalPrice(totalPrice);
 
         return room;
@@ -70,30 +77,38 @@ public class BookingService {
 
     private Hotel calculateBooking(final BrokerHotel brokerHotel, final Long days, final Integer adults,
                     final Integer child) {
+        log.info("Calculating booking: hotel {}, {} days, {} adults, {} child",
+                        brokerHotel.getName(), days, adults, child);
         final var hotel = this.hotelMapper.toDomain(brokerHotel);
         hotel.setRooms(brokerHotel.getRooms().stream()
                         .map(brokerHotelRoom -> this.calculateTotalPrice(brokerHotelRoom, days, adults, child))
                         .collect(Collectors.toList()));
 
+        log.info("Booking result {}", hotel);
         return hotel;
     }
 
     public Optional<Hotel> getHotelDetails(final Integer codeHotel) {
+        log.info("Finding hotel details: {}", codeHotel);
+
         final var hotelDetails = this.brokerService.getHotelDetails(codeHotel);
 
         if (Optional.ofNullable(hotelDetails).isPresent()) {
+            log.info("Hotel found: {}", hotelDetails);
             return Optional.of(this.calculateBooking(hotelDetails, ONE_DAY, ONE_PAX, ONE_PAX));
         }
 
+        log.info("Hotel {} not found", codeHotel);
         return Optional.empty();
     }
 
     public List<Hotel> findHotels(final Integer cityCode, final LocalDate checkin, final LocalDate checkout,
                     final Integer adults, final Integer child) {
-
+        log.info("Finding hotels: city {}, checkin {}, checkout {}", cityCode, checkin, checkout);
         final var hotels = this.brokerService.findHotelsByCity(cityCode);
         final var period = this.calculatePeriod(checkin, checkout);
 
+        log.info("Result of searching hotels: {}", hotels.size());
         return hotels.stream().map(brokerHotel -> this.calculateBooking(brokerHotel, period, adults, child))
                         .collect(Collectors.toList());
     }
--- src/main/java/br/com/cvc/evaluation/service/FeeService.java
+++ src/main/java/br/com/cvc/evaluation/service/FeeService.java
@@ -4,6 +4,9 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.math.BigDecimal;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @ApplicationScoped
 public class FeeService {
     // o ideal é que a comissão seja parametrizada via banco de dados
@@ -12,8 +15,11 @@ public class FeeService {
     FeeProvider provider;
 
     public BigDecimal calculateFee(final BigDecimal paxPrice, final Long days) {
+        log.info("Calculating fee: pax price {} for {} days", paxPrice, days);
         final var totalPricePax = paxPrice.multiply(BigDecimal.valueOf(days));
+        final var fee = totalPricePax.multiply(this.provider.fee());
 
-        return totalPricePax.multiply(this.provider.fee());
+        log.info("Fee calculated: {}", fee);
+        return fee;
     }
 }
