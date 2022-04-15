6782903768143556477638c714356cfeafbecc17
JOAO PAULO BRANDAO DA SILVA <joao.silva-ext@viavarejo.com.br>
2022-04-15 16:09:37 -0300

    Fixing unit test for rest-client

--- src/test/java/br/com/cvc/evaluation/broker/BrokerServiceTest.java
+++ src/test/java/br/com/cvc/evaluation/broker/BrokerServiceTest.java
@@ -1,21 +1,51 @@
 package br.com.cvc.evaluation.broker;
 
-import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyCollectionOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import javax.inject.Inject;
+
+import br.com.cvc.evaluation.broker.dto.BrokerHotel;
 import br.com.cvc.evaluation.extensions.WireMockExtensions;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 @QuarkusTestResource(WireMockExtensions.class)
 public class BrokerServiceTest {
-
+    @Inject
+    @RestClient
+    BrokerService brokerService;
     @Test
-    public void testGetHotelDetailsEndpoint() {
-        given()
-                        .when().get("/hotels/1")
-                        .then()
-                        .statusCode(200);
+    void testGetHotelDetailsEndpoint() {
+        final var idHotel = 1;
+
+        final var hotelDetails = this.brokerService.getHotelDetails(idHotel);
+
+        assertAll("success",
+            () -> assertNotNull(hotelDetails),
+            () -> assertThat(hotelDetails.getId(), is(idHotel))
+        );
     }
+    @Test
+    void testFindHotelsByCityEndpoint() {
+        final var codeCity = 55;
+
+        final var hotelsByCity = this.brokerService.findHotelsByCity(codeCity);
+
+        assertAll("success",
+                        () -> assertThat(hotelsByCity, not(emptyCollectionOf(BrokerHotel.class))),
+                        () -> assertThat(hotelsByCity.size(), is(2)),
+                        () -> assertTrue(hotelsByCity.stream()
+                                        .allMatch(brokerHotel -> brokerHotel.getCityCode().equals(codeCity)))
+        );
+    }
+
 }
